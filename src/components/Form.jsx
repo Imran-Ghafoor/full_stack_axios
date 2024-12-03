2c1ad53698aff79062f3bd270cd6c3fa76c2d2b2
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2024-12-03 23:14:19 +0500

    fullstack CRUD

---
+++ src/components/Form.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { postData, updateData } from "../api/PostApi";
+
+export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
+  const [addData, setAddData] = useState({
+    title: "",
+    body: "",
+  });
+
+  // update button add to edit ....
+  let isEmpty = Object.keys(updateDataApi).length === 0;
+
+  // get the update data and add into the input field
+
+  useEffect(() => {
+    updateDataApi &&
+      setAddData({
+        title: updateDataApi.title || "",
+        body: updateDataApi.body || "",
+      });
+  }, [updateDataApi]);
+
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setAddData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+  const addPostData = async () => {
+    const res = await postData(addData);
+    console.log("res", res);
+
+    if (res.status === 201) {
+      setData([...data, res.data]);
+      setAddData({ title: "", body: "" });
+    }
+  };
+  //   updatePostData
+  const updatePostData = async () => {
+    try {
+      const res = await updateData(updateDataApi.id, addData);
+      setData((prev) => {
+        return prev.map((curElem) => {
+          return curElem.id === res.data.id ? res.data : curElem;
+        });
+      });
+
+      setAddData({ title: "", body: "" });
+      setUpdateDataApi({}); // when edit button state not change into Add button
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // form submission
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const action = e.nativeEvent.submitter.value;
+
+    if (action === "Add") {
+      addPostData();
+    } else if (action === "Edit") {
+      updatePostData();
+    }
+    addPostData();
+  };
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        <label htmlFor="title"></label>
+        <input
+          type="text"
+          autoComplete="off"
+          id="title"
+          name="title"
+          placeholder="Add Title"
+          value={addData.title}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="body"></label>
+        <input
+          type="text"
+          autoComplete="off"
+          id="body"
+          name="body"
+          placeholder="Add Post"
+          value={addData.body}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button type="submit" value={isEmpty ? "Add" : "Edit"}>
+        {isEmpty ? "Add" : "Edit"}
+      </button>
+    </form>
+  );
+};
